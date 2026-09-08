373a3b203e81a60506ff27069bcc57241d8ce850
Matheus Benevides <matheusgoncalvesbenevides@gmail.com>
2026-09-08 20:11:12 -0300

    feat: arquivos Docker e base apontando pra Public com express

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Chamdando minha biblioteca de Env e Express
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+// Porta definida no .env se não for definida, vai exibir um erro e encerrar o processo
+const PORT = process.env.PORT;
+if (!PORT) {
+    console.log('A variável PORT não está definida no .env.');
+    process.exit(1);
+}
+
+// Apontando pra public que tem um index do "Em Construção" por hora
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Binbo rodando na porta ${PORT}`);
+});
