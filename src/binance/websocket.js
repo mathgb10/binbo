7b616d6210fd9b72a655d4efe4ef35c565b0126f
Matheus Benevides <matheusgoncalvesbenevides@gmail.com>
2026-09-11 21:26:21 -0300

    test: alternativa ao .us

--- src/binance/websocket.js
+++ src/binance/websocket.js
@@ -8,7 +8,7 @@ function conectar() {
     
     // Cria o Socket apontando pra Binance
     // Se a conexão falhar 3 vezes, ele tenta conectar na Binance US
-    let socket = mortes < 3 ? new webSocket('wss://stream.binance.com/ws/btcbrl@ticker') : new webSocket('wss://stream.binance.us/ws/btcbrl@ticker');
+    let socket = mortes < 3 ? new webSocket('wss://stream.binance.com/ws/btcbrl@ticker') : new webSocket('wss://data-stream.binance.vision/ws/btcbrl@ticker');
 
     // Incia o Socket
     socket.on('open', async () => {
